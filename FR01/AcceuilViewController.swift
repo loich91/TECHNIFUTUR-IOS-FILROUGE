bc658e6eecee0c487b13dd734faa51c44c1588bd
loich91 <loichubermont@yahoo.fr>
2021-06-15 11:01:21 +0200

    ajout nav barre et tal barre

---
+++ FR01/AcceuilViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AcceuilViewController.swift
+//  FR01
+//
+//  Created by Student07 on 15/06/2021.
+//
+
+import UIKit
+
+class AcceuilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.title = "ACCEUIL"
+    }
+    
+
+
+
+}
---
+++ FR01/MangerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MangerViewController.swift
+//  FR01
+//
+//  Created by Student07 on 15/06/2021.
+//
+
+import UIKit
+
+class MangerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.title = "MANGER"
+    }
+    
+
+
+
+}
